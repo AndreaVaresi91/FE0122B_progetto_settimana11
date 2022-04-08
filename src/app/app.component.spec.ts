c939671a5f86f1baeb55eeaa6614a9302107d861
AndreaVaresi91 <animadelchaos@gmail.com>
2022-04-08 14:08:09 +0200

    progetto11

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Progetto-settimana-11'`, () => {
+  it(`should have as title 'prog_sett12_start'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Progetto-settimana-11');
+    expect(app.title).toEqual('prog_sett12_start');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Progetto-settimana-11 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('prog_sett12_start app is running!');
   });
 });
